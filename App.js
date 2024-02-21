20b8eff6870bd361c4ea45a7c8e3c6f26c020aa4
Ana Reis <anajulia.2007rs@gmail.com>
2024-02-21 10:37:48 -0500

    reactive active test

--- App.js
+++ App.js
@@ -4,17 +4,39 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={[styles.numero, {color:'black'}]}>05</Text>
+    <Text style={[styles.sala, {backgroundColor:'pink'}]}>I2HN</Text>
+    <Text style={[styles.texto, {color:'blue'}]}>Ana Júlia Reis De Sousa</Text>
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  flex: 1,
+  backgroundColor: 'gray',
+  alignItems: 'center',
+  justifyContent: 'center',
+ },
+  texto: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    left: '30%',
+  },
+  numero: {
+    fontSize: 25,
+    fontStyle: 'italic',
     alignItems: 'center',
     justifyContent: 'center',
+    right: '30%',
+    top: '25%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    marginBottom: 30,
   },
-});
+  sala:{
+    fontSize: 20,
+  },
+
+  })
